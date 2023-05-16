6c5800cb88decffee530cee3cea252cbd84fc2e5
aleix10kst <acanet94@gmail.com>
2023-05-16 19:49:56 +0200

    Fix Database config using localhost in production

--- libs/api/shared/configuration/src/lib/config/database.config.ts
+++ libs/api/shared/configuration/src/lib/config/database.config.ts
@@ -2,7 +2,7 @@ import { registerAs } from '@nestjs/config';
 
 export const databaseConfig = registerAs('database', () => ({
   DATABASE_URL:
-    process.env['NODE_ENV'] !== 'development'
+    process.env['NODE_ENV'] === 'development'
       ? 'postgres://postgres:postgres@localhost:5432/postgres'
       : process.env['DATABASE_URL'],
 }));
