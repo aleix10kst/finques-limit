4c2b90e5eec2bfb21e69e89192ccd49efe753c6d
aleix10kst <acanet94@gmail.com>
2023-05-16 19:22:24 +0200

    Fix database config issue

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -13,7 +13,6 @@ async function bootstrap() {
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
-  console.log(process.env.NODE_ENV);
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
--- libs/api/shared/database/src/lib/database.module.ts
+++ libs/api/shared/database/src/lib/database.module.ts
@@ -5,15 +5,22 @@ import { databaseConfig } from '@finques-limit/api/shared/configuration';
 
 @Module({
   imports: [
-    ConfigModule.forFeature(databaseConfig),
     TypeOrmModule.forRootAsync({
+      imports: [ConfigModule.forFeature(databaseConfig)],
       inject: [ConfigService],
-      useFactory: (configService: ConfigService) => {
-        console.log(configService);
-        return {
-          type: 'postgres',
-        };
-      },
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get('database.DATABASE_URL'),
+        ssl: true,
+        autoLoadEntities: true,
+        // TODO: Delete once the app is released
+        synchronize: true,
+        extra: {
+          ssl: {
+            rejectUnauthorized: false,
+          },
+        },
+      }),
     }),
   ],
   exports: [TypeOrmModule],
